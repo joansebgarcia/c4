454ea476f335166c3e15703f90da99aef9cbb667
Sebastián García <sebastiandotdev@gmail.com>
2026-07-16 17:16:21 -0500

    test(hello): port test suite to c++

--- tests/test_hello.cc
+++ tests/test_hello.cc
@@ -1,12 +1,12 @@
-#include <assert.h>
-#include <string.h>
+#include <cassert>
+#include <cstring>
 
 #include "c4/c4.h"
 
-int main(void) {
-  const char* result = c4_hello();
-  assert(strcmp(result, "Hello, World!") == 0);
-  if (strcmp(result, "Hello, World!") != 0) {
+int main() {
+  const char* result = c4::Hello();
+  assert(std::strcmp(result, "Hello, World!") == 0);
+  if (std::strcmp(result, "Hello, World!") != 0) {
     return 1;
   }
   return 0;
